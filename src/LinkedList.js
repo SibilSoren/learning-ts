46122d35010bd5af2a70bd4cf42ad06cc2c678a9
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2023-09-19 00:32:23 +0530

    Insert funtion added

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -20,10 +27,40 @@ class LinkedList {
   prepend = (value) => {
     let newNode = {
       value: value,
-      next: this.head,
+      next: null,
     };
+    newNode.next = this.head;
     this.head = newNode;
     this.length++;
+  };
+  printList = () => {
+    const arr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return arr;
+  };
+  insert = (index, value) => {
+    // let i = 0;
+    let newNode = new Node(value);
+    let currentNode = this.head;
+    if (index === 0) {
+      this.prepend(value);
+    }
+    if (index >= this.length) {
+      this.append(value);
+    }
+    for (let i = 0; i <= index; i++) {
+      //   console.log("crr", currentNode);
+      if (i === index - 1) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
   };
 }
 
@@ -31,4 +68,6 @@ let myLL = new LinkedList(10);
 myLL.append(15);
 myLL.append(25);
 myLL.prepend(35);
-console.log(myLL);
+console.log(myLL.printList());
+myLL.insert(0, 99);
+console.log(myLL.printList());
